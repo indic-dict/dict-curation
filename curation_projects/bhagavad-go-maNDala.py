c0bc3c3a7fd6ed1db2c3ceaa8b585634bb9ec2ec
vvasuki <vishvas.vasuki@gmail.com>
2020-05-26 16:33:43 +0530

    CU

--- curation_projects/bhagavad-go-maNDala.py
+++ curation_projects/bhagavad-go-maNDala.py
@@ -1,19 +1,48 @@
+import codecs
+import logging
+import os
+
+from selenium.webdriver.support.select import Select
+
 from curation_utils import scraping
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
+
+
 
 letters = "અ આ ઇ ઈ ઉ ઊ ઋ ઌ ઍ એ ઐ ઑ ઓ ઔ ક ખ ગ ઘ ઙ ચ છ જ ઝ ઞ ટ ઠ ડ ઢ ણ ત થ દ ધ ન પ ફ બ ભ મ ય ર લ ળ વ શ ષ સ હ ૐ ૠ ૡ".split()
 
 
 browser = scraping.get_selenium_browser(headless=False)
 
-def get_headwords(letter):
+def get_letter_headwords(letter, file_out):
     url = "http://www.bhagwadgomandal.com/index.php?action=dictionary&sitem=%s&type=1&page=0" % letter
+    logging.info("Processing %s", letter)
     browser.get(url=url)
-    page_dropdown = browser.find_element_by_css_selector("select[name=pgInd]")
-    page_dropdown_length = len(browser.find_element_by_css_selector("select[name=pgInd] option"))
+    page_dropdown = browser.find_element_by_name("pgInd")
+    page_options = list(page_dropdown.find_elements_by_css_selector("option"))
+    logging.info("Number of pages: %d", len(page_options))
+    browser.implicitly_wait(500)
 
-    for option_index in range(1, page_dropdown_length+1):
-        page_dropdown.select_by_index(option_index)
+    for option_index in range(1, len(page_options)+1):
+        logging.info("Page %d", option_index)
+        page_dropdown = browser.find_element_by_name("pgInd")
+        word_elements = browser.find_elements_by_css_selector("a.word")
+        words = [w.text for w in word_elements]
+        file_out.write("\n".join(words) + "\n")
+        Select(page_dropdown).select_by_index(option_index)
+
+
+def get_headwords(out_path):
+    os.makedirs(os.path.dirname(out_path))
+    with codecs.open(out_path, "w", 'utf-8') as file_out:
+        for letter in letters:
+            get_letter_headwords(letter=letter, file_out=file_out)
 
 
 if __name__ == '__main__':
-    get_headwords("અ")
+    get_headwords(out_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/bhagavad-go-maNDala/mUlam/headwords.csv")
