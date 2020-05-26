a5bfdaa160d393b914cc688584a12687f0ad827c
vvasuki <vishvas.vasuki@gmail.com>
2020-05-26 19:23:00 +0530

    CU

--- curation_projects/bhagavad-go-maNDala.py
+++ curation_projects/bhagavad-go-maNDala.py
@@ -1,6 +1,8 @@
 import codecs
 import logging
 import os
+from functools import partial
+from multiprocessing import Pool
 
 from indic_transliteration import sanscript
 from selenium.webdriver.support.select import Select
@@ -18,34 +20,44 @@ logging.basicConfig(
 letters = "અ આ ઇ ઈ ઉ ઊ ઋ ઌ ઍ એ ઐ ઑ ઓ ઔ ક ખ ગ ઘ ઙ ચ છ જ ઝ ઞ ટ ઠ ડ ઢ ણ ત થ દ ધ ન પ ફ બ ભ મ ય ર લ ળ વ શ ષ સ હ ૐ ૠ ૡ".split()
 
 
-browser = scraping.get_selenium_browser(headless=False)
 
-def get_letter_headwords(letter, file_out):
-    url = "http://www.bhagwadgomandal.com/index.php?action=dictionary&sitem=%s&type=1&page=0" % letter
-    logging.info("Processing %s", letter)
-    browser.get(url=url)
-    page_dropdown = browser.find_element_by_name("pgInd")
-    page_options = list(page_dropdown.find_elements_by_css_selector("option"))
-    logging.info("Number of pages: %d", len(page_options))
-    browser.implicitly_wait(500)
-
-    for option_index in range(1, len(page_options)+1):
-        logging.info("Page %s %d", letter, option_index)
+def get_letter_headwords(letter, out_path_dir):
+    browser = scraping.get_selenium_browser(headless=True)
+    out_path = os.path.join(out_path_dir, letter + ".csv")
+    if os.path.exists(out_path):
+        logging.warning("Skipping %s as %s exists", letter, out_path)
+        return 0 
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    with codecs.open(out_path, "w", 'utf-8') as file_out:
+        url = "http://www.bhagwadgomandal.com/index.php?action=dictionary&sitem=%s&type=1&page=0" % letter
+        logging.info("Processing %s", letter)
+        browser.get(url=url)
         page_dropdown = browser.find_element_by_name("pgInd")
-        word_elements = browser.find_elements_by_css_selector("a.word")
-        words = [w.text for w in word_elements]
-        file_out.write("\n".join(words) + "\n")
-        Select(page_dropdown).select_by_index(option_index)
+        page_options = list(page_dropdown.find_elements_by_css_selector("option"))
+        logging.info("Number of pages: %d", len(page_options))
+        browser.implicitly_wait(250)
+    
+        word_count = 0 
+        for option_index in range(0, len(page_options)):
+            Select(page_dropdown).select_by_index(option_index)
+            logging.info("Page %s %d", letter, option_index)
+            page_dropdown = browser.find_element_by_name("pgInd")
+            word_elements = browser.find_elements_by_css_selector("a.word")
+            words = [w.text for w in word_elements]
+            word_count = word_count + len(words)
+            file_out.write("\n".join(words) + "\n")
+            
+        browser.close()
+        return word_count
 
 
 def get_headwords(out_path):
-    os.makedirs(os.path.dirname(out_path))
-    with codecs.open(out_path, "w", 'utf-8') as file_out:
-        for letter in letters:
-            get_letter_headwords(letter=letter, file_out=file_out)
+    pool = Pool(4)
+    f = partial(get_letter_headwords, out_path_dir=out_path)
+    pool.map(f, letters)
 
 
-def get_definition(headword):
+def get_definition(headword, browser):
     url = "http://www.bhagavadgomandal.com/index.php?action=dictionary&sitem=%s&type=3&page=0" % headword
     browser.get(url=url)
     for detail_link in browser.find_elements_by_css_selector("a.detaillink"):
@@ -60,8 +72,9 @@ def get_definition(headword):
 
 
 def dump_definitions(in_path, out_path, out_path_devanagari_entries):
-    os.makedirs(os.path.dirname(out_path))
-    os.makedirs(os.path.dirname(out_path_devanagari_entries))
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    os.makedirs(os.path.dirname(out_path_devanagari_entries), exist_ok=True)
+    browser = scraping.get_selenium_browser(headless=True)
 
     with codecs.open(in_path, "r", 'utf-8') as file_in, codecs.open(out_path, "w", 'utf-8') as file_out, codecs.open(out_path_devanagari_entries, "w", 'utf-8') as file_out_devanagari:
         while True:
@@ -70,12 +83,12 @@ def dump_definitions(in_path, out_path, out_path_devanagari_entries):
                 break
             headword = headword.strip().replace(":", "ઃ")
             devanagari_headword = sanscript.transliterate(data=headword, _from=sanscript.GUJARATI, _to=sanscript.DEVANAGARI)
-            definition = get_definition(headword=headword)
+            definition = get_definition(headword=headword, browser=browser)
             definition_devanagari = sanscript.transliterate(data=definition, _from=sanscript.GUJARATI, _to=sanscript.DEVANAGARI)
             file_out.writelines(["%s|%s\n%s\n\n" % (headword, devanagari_headword, definition)])
             file_out_devanagari.writelines(["%s|%s\n%s\n\n" % (headword, devanagari_headword, definition_devanagari)])
 
 
 if __name__ == '__main__':
-    # get_headwords(out_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/bhagavad-go-maNDala/mUlam/headwords.csv")
-    dump_definitions(in_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/bhagavad-go-maNDala/mUlam/headwords.csv", out_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/gu-entries/bhagavad-go-maNDala/bhagavad-go-maNDala.babylon", out_path_devanagari_entries="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/dev-entries/bhagavad-go-maNDala/bhagavad-go-maNDala.babylon")
+    get_headwords(out_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/bhagavad-go-maNDala/mUlam/")
+    # dump_definitions(in_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/bhagavad-go-maNDala/mUlam/headwords.csv", out_path="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/gu-entries/bhagavad-go-maNDala/bhagavad-go-maNDala.babylon", out_path_devanagari_entries="/home/vvasuki/indic-dict/stardict-gujarati/gu-head/dev-entries/bhagavad-go-maNDala/bhagavad-go-maNDala.babylon")
