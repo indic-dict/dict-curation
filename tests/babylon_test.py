32d6d4e8ce3dbf80ab21e060af7e69438bd5664f
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-01-22 12:14:27 +0530

    cu

--- tests/babylon_test.py
+++ tests/babylon_test.py
@@ -1,15 +1,13 @@
 import logging
 import os
 
-import dict_curation.babylon.definitions
-from dict_curation import babylon
-
+from dict_curation.babylon import definitions_helper
 
 test_babylon = os.path.join(os.path.dirname(__file__), "test.babylon")
 
 
 def test_get_existing_definitions():
-    definitions = dict_curation.babylon.definitions.get_definitions(test_babylon)
+    definitions = definitions_helper.get_definitions(test_babylon)
     headword = 'આપોશાન'
     assert headword in definitions
     logging.info(definitions[headword])
