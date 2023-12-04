116163d44d6b521c8d91da15c2174daa7e5edfbc
vvasuki <vishvas.vasuki@gmail.com>
2023-12-04 08:50:07 +0530

    CU

---
+++ dict_curation_projects/vidyut-calls.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+#cargo run --release --example create_tinantas_babylon -- --sanadi san --desc "vidyud-yantreRa janitAni sannteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san/vidyut-san.babylon
+#cargo run --release --example create_tinantas_babylon -- --sanadi san --prayoga karmani --desc "vidyud-yantreRa janitAni sannteByas tiNantAni (karmaRi)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san-karmaNi/vidyut-san-karmaNi.babylon
+
+#cargo run --release --example create_tinantas_babylon -- --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich/vidyut-Nich.babylon
+#cargo run --release --example create_tinantas_babylon -- --prayoga karmani --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni (karmaRi)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich-karmaNi/vidyut-Nich-karmaNi.babylon
+
+#cargo run --release --example create_tinantas_babylon -- --sanadi yaN --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-karmaNi/vidyut-yaN-karmaNi.babylon
+#cargo run --release --example create_tinantas_babylon -- --sanadi yaN --prayoga karmani --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni (karmaRi)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-karmaNi/vidyut-yaN-karmaNi.babylon
+
+#cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-karmaNi/vidyut-yaN-luk.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --prayoga karmani --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni (karmaRi)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-karmaNi/vidyut-yaN-luk-karmaNi.babylon
