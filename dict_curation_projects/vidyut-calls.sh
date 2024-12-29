5344bfa1755de7fce4aa4b57284d12674707c1de
vvasuki <vishvas.vasuki@gmail.com>
2024-12-29 08:16:54 +0530

    अस्द्फ़्

--- dict_curation_projects/vidyut-calls.sh
+++ dict_curation_projects/vidyut-calls.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 #mkdir vidyut-tiN
 #mkdir vidyut-tiN-akartari
@@ -11,18 +11,20 @@
 #mkdir vidyut-yaN-luk-tiN  
 #mkdir vidyut-yaN-luk-akartari
 
-cargo run --release --example create_tinantas_babylon -- --desc "vidyud-yantreRa janitAni tiNantAni " > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-tiN/vidyut-tiNanta.babylon
-cargo run --release --example create_tinantas_babylon -- --prayoga karmani --desc "vidyud-yantreRa janitAni tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-tiN-akartari/vidyut-tiNanta-akartari.babylon
+cd /home/vvasuki/gitland/ambuda-org/vidyut/vidyut-prakriya/
+
+cargo run --release --example create_tinantas_babylon -- --desc "vidyud-yantreRa janitAni tiNantAni " > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-tiN/vidyut-tiN.babylon
+cargo run --release --example create_tinantas_babylon -- --prayoga karmani --desc "vidyud-yantreRa janitAni tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-tiN-akartari/vidyut-tiN-akartari.babylon
 
 
-cargo run --release --example create_tinantas_babylon -- --sanadi san --desc "vidyud-yantreRa janitAni sannteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san-tiN/vidyut-san-tiN.babylon
-cargo run --release --example create_tinantas_babylon -- --sanadi san --prayoga karmani --desc "vidyud-yantreRa janitAni sannteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san-akartari/vidyut-san-akartari.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi san --desc "vidyud-yantreRa janitAni sannteByas tiNantAni" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san-tiN/vidyut-san-tiN.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi san --prayoga karmani --desc "vidyud-yantreRa janitAni sannteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-san-akartari/vidyut-san-akartari.babylon
 
-cargo run --release --example create_tinantas_babylon -- --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich-tiN/vidyut-Nich-tiN.babylon
-cargo run --release --example create_tinantas_babylon -- --prayoga karmani --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich-akartari/vidyut-Nich-akartari.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich-tiN/vidyut-Nich-tiN.babylon
+cargo run --release --example create_tinantas_babylon -- --prayoga karmani --sanadi Ric --desc "vidyud-yantreRa janitAni RicanteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-Nich-akartari/vidyut-Nich-akartari.babylon
 
-cargo run --release --example create_tinantas_babylon -- --sanadi yaN --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-tiN/vidyut-yaN-tiN.babylon
-cargo run --release --example create_tinantas_babylon -- --sanadi yaN --prayoga karmani --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-akartari/vidyut-yaN-akartari.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi yaN --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-tiN/vidyut-yaN-tiN.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi yaN --prayoga karmani --desc "vidyud-yantreRa janitAni yaNNanteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-akartari/vidyut-yaN-akartari.babylon
 
-cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-tiN/vidyut-yaN-luk-tiN.babylon
-cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --prayoga karmani --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict_stardict/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-akartari/vidyut-yaN-luk-akartari.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-tiN/vidyut-yaN-luk-tiN.babylon
+cargo run --release --example create_tinantas_babylon -- --sanadi yaNluk --prayoga karmani --desc "vidyud-yantreRa janitAni yaNluganteByas tiNantAni (akartari)" > /home/vvasuki/gitland/indic-dict/dicts/stardict-sanskrit-vyAkaraNa/tiNanta/vidyut/vidyut-yaN-luk-akartari/vidyut-yaN-luk-akartari.babylon
